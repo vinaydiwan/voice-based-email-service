2d4c122b64f2b071973212aec45b787327356bd4
vinay diwan <vinaydwn99@gmail.com>
2021-08-06 15:09:19 +0530

    minor change

--- src/app.js
+++ src/app.js
@@ -18,10 +18,11 @@ const localStrategy = require('passport-local')
 const User = require("../models/users")
 const mongoSanitize = require("express-mongo-sanitize")
 const helmet = require("helmet")
+const MongoStore = require('connect-mongo')
 
 // connecting server to database
 //const dbURL = process.env.DB_URL;
-const dbURL = process.env.DB_URL || 'mongodb://localhost/VoiceBasedEmail'
+const dbURL = process.env.DB_URL
 mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -41,13 +42,18 @@ const secret = process.env.SECRET || "asjbdkdba";
 app.use(session({
     name: "_gla",
     secret,
+    store: MongoStore.create({
+        mongoUrl: dbURL,
+        secret,
+        touchAfter: 24 * 3600,
+    }),
     resave: false,
     saveUninitialized: true,
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
         expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
         httpOnly: true,
-        secure : true,
+        secure: true,
     }
 }))
 
@@ -80,5 +86,5 @@ app.use((err, req, res, next) => {
 })
 const port = process.env.PORT || 4000
 app.listen(port, () => {
-    console.log("server on port ",port)
+    console.log("server on port ", port)
 })
